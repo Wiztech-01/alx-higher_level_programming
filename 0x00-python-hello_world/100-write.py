213f568291390e02c31520dff7d30547621af315
Wiztech-01 <lynnnyaoke@gmail.com>
2023-10-03 02:37:16 +0000

    clear

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-from sys import stderr
-stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-exit(1)
+import sys
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write(message + "\n")
+sys.exit(1)
