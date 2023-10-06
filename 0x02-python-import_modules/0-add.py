1a4d3654ca1c61070c56b8d2f4cc4bc8358e16ef
Wiztech-01 <lynnnyaoke@gmail.com>
2023-10-06 02:47:00 +0000

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-x = 1
-y = 2
+a = 1
+b = 2
 
 if __name__ == "__main__":
     from add_0 import add
-print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 
